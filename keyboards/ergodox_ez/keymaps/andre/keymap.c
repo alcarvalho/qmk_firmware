7f89cd4e759dc6ddc3ac9f0625182ff615ab2c20
Andre Carvalho <carvalho@work.co>
2018-08-19 19:57:53 -0700

    Win layer should use right alt

--- keyboards/ergodox_ez/keymaps/andre/keymap.c
+++ keyboards/ergodox_ez/keymaps/andre/keymap.c
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                                            KC_LCTRL, KC_TRNS,
                                                      KC_TRNS,
-                                  KC_TRNS, KC_TRNS,  KC_TRNS,
+                                  KC_TRNS, KC_RALT,  KC_TRNS,
     // right hand
        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
